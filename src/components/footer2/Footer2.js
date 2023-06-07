eb686392160bca6d13470de1e2c8615f3b38107f
Maureen <morynmoh@gmail.com>
2023-06-07 16:43:59 +0300

    footer

---
+++ src/components/footer2/Footer2.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer2.css';
+
+
+const Footer2 = () => {
+  return (
+    <div className="footer2">
+      <p>@ 2023 Copyright by Moringa Group 4 Software Development </p>
+      <p>Dyphnah | Mike | Kenneth | Abinel | Maureen </p>
+    </div>
+  )
+}
+
+export default Footer2
